8d776f901829c35961339fbbcd1c0afd5c8b29ea
D. Yamaguchi <yamadaiji73@gmail.com>
2026-02-03 16:42:56 +0900

    update

---
+++ src/main/java/myprototype/jparse/StateKey.java
@@ -0,0 +1,14 @@
+package myprototype.jparse;
+
+import java.util.Collection;
+import java.util.List;
+
+public class StateKey {
+	public StateKey(Collection<Item> items) {
+		
+	}
+	
+	public List<StateKey> derivativeKey() {
+		
+	}
+}
